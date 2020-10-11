9ab8a4188512a1441187acba720733486f2bb33c
William-McGonagle <mcgonaglew@gfacademy.org>
2020-10-11 16:52:36 -0400

    Added the Hardware Configs.
    Signed-off-by: William-McGonagle <mcgonaglew@gfacademy.org>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
@@ -6,21 +6,45 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ReadWriteFile;
 
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.module.Bartender;
 import org.firstinspires.ftc.teamcode.module.Input;
+
+import java.io.File;
 
 @TeleOp(name = "Autonomous Code", group = "code")
 public class AutoCode extends LinearOpMode {
 
-    // I wish this was C# because the code would be so much cleaner
-    @Override public void runOpMode() {
+    public DcMotor rightFront;
+    public DcMotor leftFront;
+    public DcMotor rightRear;
+    public DcMotor leftRear;
 
-        // Initialize the Bartender/ Check if Initializing Failed
+    @Override
+    public void runOpMode() {
+
+        // Log Status to Console.
+        telemetry.addData("Status", "Initializing Hardware.");
+        System.out.println("Initializing Hardware.");
+        telemetry.update();
+
+        // Initialize the Hardware/ Check if Initializing Failed.
+        if (!InitializeHardware()) {
+
+            FailTeleop("Failed to Initialize the Hardware.");
+            return;
+
+        }
+
+        // Log Status to Console.
+        telemetry.addData("Status", "Initializing Bartender.");
+        System.out.println("Initializing Bartender.");
+        telemetry.update();
+
+        // Initialize the Bartender/ Check if Initializing Failed.
         if (!InitializeBartender()) {
 
             FailTeleop("Failed to Initialize the Bartender.");
@@ -28,7 +52,12 @@ public class AutoCode extends LinearOpMode {
 
         }
 
-        // Initialize Modules/ Check if Initializing Failed
+        // Log Status to Console.
+        telemetry.addData("Status", "Initializing Modules.");
+        System.out.println("Initializing Modules.");
+        telemetry.update();
+
+        // Initialize Modules/ Check if Initializing Failed.
         if (!InitializeModules()) {
 
             FailTeleop("Failed to Initialize the Modules.");
@@ -38,19 +67,81 @@ public class AutoCode extends LinearOpMode {
 
     }
 
-    // Log the Failures
+    /**
+     * Logs the reason why the robot's code has crashed, saves the logs of the robot, and exits the main loop.
+     * @param error The reason for why the robot has crashed.
+     * @since ∞ + 1
+     * **/
     public void FailTeleop(String error) {
 
         // Log the problem in the console.
         System.out.println("Robot Error: " + error);
 
         // Log the problem to the robot console
-
+        telemetry.addData("Status", "Robot Crashed Due to Error.");
+        telemetry.addData("Error", error);
+        telemetry.update();
 
         // Save the current log to a file.
+        File errorLog = AppUtil.getInstance().getSettingsFile("errorLog.txt");
+
+        String fileContents = "##########\n" +
+                "# NEW LOG #\n" +
+                "##########\n" +
+                "\n" +
+                "Error: " + error + "\n\n";
+
+        ReadWriteFile.writeFile(errorLog, fileContents);
 
     }
 
+    /**
+     * Initialize the Hardware of the Robot.
+     * @return  <code>true</code> The initialization of the robot's hardware was a success.
+     *          <code>false</code> The initialization of the robot's hardware has failed.
+     * @since ∞ + 1
+     * **/
+    public boolean InitializeHardware() {
+
+        // Initialize all of the motors for the cart.
+        rightFront = hardwareMap.dcMotor.get("rf");
+        leftFront = hardwareMap.dcMotor.get("lf");
+        rightRear = hardwareMap.dcMotor.get("rr");
+        leftRear = hardwareMap.dcMotor.get("lr");
+
+        // Stop and reset all of the motors- for peace of mind.
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Set all of the run modes to be without encoders.
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Set all of the motors to brake when they do not have power.
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Log output to console
+        telemetry.addData("Status", "Successfully Initiated Hardware");
+        telemetry.update();
+
+        // Since everything worked, return true.
+        return true;
+
+    }
+
+    /**
+     * Initialize the Bartender object- this will allow for the drinks to be loaded into the robot.
+     * @return  <code>true</code> The initialization of the Bartender was a success.
+     *          <code>false</code> The initialization of the Bartender has failed.
+     * @since ∞ + 1
+     * **/
     public boolean InitializeBartender() {
 
         // Create Singleton
@@ -59,16 +150,23 @@ public class AutoCode extends LinearOpMode {
         // Add Drinks to Bartender
             // USAGE: Bartender.Singleton.addDrink(drink);
 
-        // Return True
+        // Since everything worked, return true.
         return true;
 
     }
 
+    /**
+     * Initialize the modules- this will allow for the drinks to access functions and info outside of themselves.
+     * @return  <code>true</code> The initialization of the modules was a success.
+     *          <code>false</code> The initialization of the modules has failed.
+     * @since ∞ + 1
+     * **/
     public boolean InitializeModules() {
 
         // Initialize Modules
         if (Input.Singleton == null) Input.Singleton = new Input();
 
+        // Since everything worked, return true.
         return true;
 
     }
