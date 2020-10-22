8fa44c52867556f3a1bd1aae563a7b2f54e89a13
groverj <groverj@gfacademy.org>
2020-10-22 17:15:58 -0400

    Coppied the axis and motor control components of the manual controller to the auto controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drink/MechanimAutoController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drink/MechanimAutoController.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.drink;
 
+import org.firstinspires.ftc.teamcode.module.Input;
+
+import java.util.Hashtable;
+
 public class MechanimAutoController extends DrinkObject{
+    float reducedSpeedModifier = 0.4f;
+
+    @Override
+    public Hashtable<String, Object> Pipe(Hashtable<String, Object> input) {
+
+        // Initialize the Hashtable
+        Hashtable<String, Object> output = input;
+
+        // Get the Input Axises
+        float drive = Input.Singleton.getAxis("Vertical");
+        float strafe = Input.Singleton.getAxis("Horizontal");
+        float rotate = Input.Singleton.getAxis("Rotation");
+        float reducedSpeed = Input.Singleton.getButton("Slow") ? reducedSpeedModifier : 1f;
+
+        // Put the output into the motors
+        output.put("motorLFOutput", reducedSpeed * (drive + strafe + rotate));
+        output.put("motorLFOutput", reducedSpeed * (drive + strafe + rotate));
+        output.put("motorLFOutput", reducedSpeed * (drive + strafe + rotate));
+        output.put("motorLFOutput", reducedSpeed * (drive + strafe + rotate));
+
+        // Return the Output
+        return output;
+
+    }
 }
