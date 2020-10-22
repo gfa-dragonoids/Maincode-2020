f9645edc1e486f7faec34fd5d365d41483eec224
groverj <groverj@gfacademy.org>
2020-10-22 17:12:48 -0400

    Optimized the imports of the auto controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drink/MechanimAutoController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drink/MechanimAutoController.java
@@ -1,8 +1,4 @@
 package org.firstinspires.ftc.teamcode.drink;
 
-import org.firstinspires.ftc.teamcode.module.Input;
-
-import java.util.Hashtable;
-
 public class MechanimAutoController extends DrinkObject{
 }
